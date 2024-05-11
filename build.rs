28277c89286b5d5437b8228631f1fcd3f1cb605b
nxcy <nxcy@example.com>
2024-05-11 04:03:42 +0000

    optimize binding generation

--- build.rs
+++ build.rs
@@ -1,11 +1,16 @@
 use std::env;
 use std::path::PathBuf;
 
+use bindgen::{EnumVariation, NonCopyUnionStyle};
+
 fn main() {
     pkg_config::probe_library("liburing-ffi").unwrap();
     bindgen::builder()
         .header("ffi.c")
-        .allowlist_function("io_uring.*")
+        .allowlist_file("/usr/include/liburing.h")
+        .allowlist_file("/usr/include/liburing/io_uring.h")
+        .default_non_copy_union_style(NonCopyUnionStyle::ManuallyDrop)
+        .default_enum_style(EnumVariation::ModuleConsts)
         .generate()
         .unwrap()
         .write_to_file(PathBuf::from(env::var("OUT_DIR").unwrap()).join("bindings.rs"))
